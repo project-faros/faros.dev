f6e15f98f0a211a82e07dfaea414b495b8b45588
Ryan Kraus <rmkraus@gmail.com>
2020-07-11 17:05:54 -0400

    Changed documentation version to just major.minor.

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Project Faros Team'
 author = 'Project Faros Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.5'
 
 
 # -- General configuration ---------------------------------------------------
