87e055a81beb4eb30b61ee4353a07f12434f03e5
Ryan Kraus <rmkraus@gmail.com>
2020-08-09 23:12:50 -0400

    Bumped version to match cluster-manager.

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Project Faros Team'
 author = 'Project Faros Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5'
+release = '0.6'
 
 
 # -- General configuration ---------------------------------------------------
